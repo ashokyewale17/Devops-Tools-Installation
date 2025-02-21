237d181e486bb2bbe767a7a40270097d8279d44f
Ashok Yewale <102237389+ashokyewale17@users.noreply.github.com>
2025-02-21 15:10:22 +0530

    Update create-eks-cluster.sh

--- EKSCTL/create-eks-cluster.sh
+++ EKSCTL/create-eks-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-eksctl create cluster --name=wanderlust --region=us-east-1 --without-nodegroup
+eksctl create cluster --name=ashokay --region=us-east-1 --without-nodegroup
 
-eksctl utils associate-iam-oidc-provider --region us-east-1 --cluster wanderlust --approve
+eksctl utils associate-iam-oidc-provider --region us-east-1 --cluster ashokay --approve
 
-eksctl create nodegroup --cluster=wanderlust --region=us-east-1 --node-type=t2.medium --nodes-min=2 --nodes-max=2 --node-volume-size=28
+eksctl create nodegroup --cluster=ashokay --region=us-east-1 --node-type=t2.medium --nodes-min=2 --nodes-max=2 --node-volume-size=28
